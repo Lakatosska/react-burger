b845fcdb17fbc1bd1e17ed6722e964bec0b65c40
Lakatosska <vizualnoe@gmail.com>
2022-03-22 10:29:08 +0300

    some layout amendments in burger constructor

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,52 +1,49 @@
 import React from "react";
 
 import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import bun02 from '../../images/bun-02.png'
+import bun02 from '../../images/bun-02.png';
+import icon from '../../images/burger-constructor_icon.svg';
 
 import burgerConstructorStyles from './burger-constructor.module.css';
 
 const ConstructorItems = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      <ConstructorElement
-        type="top"
-        isLocked={true}
-        text="Краторная булка N-200i (верх)"
-        price={200}
-        //thumbnail={img}
-        thumbnail={bun02}
-      />
-      <ConstructorElement
-        text="Краторная булка N-200i (верх)"
-        price={50}
-        //thumbnail={img}
-        thumbnail={bun02}
-      />
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text="Краторная булка N-200i (низ)"
-        price={200}
-        //thumbnail={img}
-        thumbnail={bun02}
-      />
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text="Краторная булка N-200i (низ)"
-        price={200}
-        //thumbnail={img}
-        thumbnail={bun02}
-      />
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text="Краторная булка N-200i (низ)"
-        price={200}
-        //thumbnail={img}
-        thumbnail={bun02}
-      />
-    </div>
+    <ul className={burgerConstructorStyles.items}>
+      <li className={burgerConstructorStyles.item}>
+        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          //thumbnail={img}
+          thumbnail={bun02}
+        />
+      </li>
+      <li className={burgerConstructorStyles.item}>
+        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          //thumbnail={img}
+          thumbnail={bun02}
+        />
+      </li>
+      <li className={burgerConstructorStyles.item}>
+        <img src={icon} alt='иконка меню' className={`${burgerConstructorStyles.icon} mr-2`}/>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          //thumbnail={img}
+          thumbnail={bun02}
+        />
+      </li>
+    
+    </ul>
   );
 }
 
@@ -54,8 +51,8 @@ const ConstructorItems = () => {
 const Order = () => {
   return(
     <div className={`${burgerConstructorStyles.order} mt-10`}>
-      <div>
-        <span>610</span>
+      <div className={`${burgerConstructorStyles.price} mr-10`}>
+        <span className="text text_type_digits-medium mr-4">610</span>
         <CurrencyIcon type="primary" />
       </div>
       <Button type="primary" size="large">
@@ -67,10 +64,10 @@ const Order = () => {
 
 const BurgerConstructor = () => {
   return(
-    <div className={`${burgerConstructorStyles.main} mt-25`}>
+    <section className={`${burgerConstructorStyles.main} mt-25`}>
       <ConstructorItems />
       <Order />
-    </div>
+    </section>
   );
 }
 
