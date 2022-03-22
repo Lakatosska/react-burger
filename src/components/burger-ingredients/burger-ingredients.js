8c2256e47083b5e34d823963ff8afed8dd9256d3
Lakatosska <vizualnoe@gmail.com>
2022-03-22 23:50:29 +0300

    received data on BurgerIngredients component

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -4,46 +4,57 @@ import { Tab, Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger
 
 import burgerIngredientsStyles from './burger-ingredients.module.css';
 
-import bun02 from '../../images/bun-02.png'
+import { data } from '../../utils/data.js';
+
+//import bun02 from '../../images/bun-02.png'
+
 
 
 
 const BurgerTabs = () => {
   const [current, setCurrent] = React.useState('one')
     return (
-      <>
-        <div style={{ display: 'flex' }}>
-          <Tab value='one' active={current === 'one'} onClick={setCurrent}>
-            Булки
-          </Tab>
-          <Tab value='two' active={current === 'two'} onClick={setCurrent}>
-            Соусы
-          </Tab>
-          <Tab value='three' active={current === 'three'} onClick={setCurrent}>
-            Начинки
-          </Tab>
-        </div>
-      </>
+      <div style={{ display: 'flex' }}>
+        <Tab value='one' active={current === 'one'} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value='two' active={current === 'two'} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab value='three' active={current === 'three'} onClick={setCurrent}>
+          Начинки
+        </Tab>
+      </div>
     );
 }
 
-const Card = ({ image, price, name }) => {
+const Card = ({ cardData }) => {
+  const { image, price, name } = cardData;
   return(
     <article className={burgerIngredientsStyles.card}>
-      
-      <img src={bun02} alt={name} className='ml-4 mr-4 mb-1'/>
+      <img src={image} alt={name} className='ml-4 mr-4 mb-1'/>
       <div className={`${burgerIngredientsStyles.priceItem} mt-1 mb-1`}>
-        <span className='mr-1'>{price}20</span>
+        <span className='mr-1'>{price}</span>
         <CurrencyIcon type='primary' />
       </div>
-      <span className={burgerIngredientsStyles.name}>{name}Краторная булка</span>
+      <span className={burgerIngredientsStyles.name}>{name}</span>
       <Counter count={1} size="default" />
     </article>
   );
 }
 
+const MenuList = (props) => {
+  const typeData = data.filter(item => item.type === props.type);
+  return(
+    <ul className={burgerIngredientsStyles.menuItems}>
+      {typeData.map(item => (
+        <Card key={item._id} cardData={item} />
+      ))}
+    </ul>
+  )
+}
 
-const BurgerIngredients = ({ data }) => {
+const BurgerIngredients = () => {
   return(
     <section className={burgerIngredientsStyles.main}>
       <h1>Соберите бургер</h1>
@@ -51,30 +62,15 @@ const BurgerIngredients = ({ data }) => {
       <ul className={burgerIngredientsStyles.menuList}>
         <li>
           <h2>Булки</h2>
-          <div className={burgerIngredientsStyles.menuItems}>
-            <Card />
-            <Card />
-          </div>
+          <MenuList type='bun' />
         </li>
         <li>
           <h2>Соусы</h2>
-          <div className={burgerIngredientsStyles.menuItems}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
+          <MenuList type='sauce' />
         </li>
         <li>
           <h2>Начинки</h2>
-          <div className={burgerIngredientsStyles.menuItems}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
+          <MenuList type='main' />
         </li>
       </ul>
     </section>
