ebb1d4fc20164069001105d00103a3e9a0f8425a
Lakatosska <vizualnoe@gmail.com>
2022-03-27 23:57:02 +0300

    added modal file structure and baseUrl, checkResponse

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,6 +6,17 @@ import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 import appStyles from './app.module.css';
 
+// https://norma.nomoreparties.space/api/ingredients
+
+const baseUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json()
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
 const App = () => {
   return(
     <div className={appStyles.app}>
