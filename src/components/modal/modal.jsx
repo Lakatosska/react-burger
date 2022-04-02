fbc30a558ca22057f0df619808493e75a1971d31
Lakatosska <vizualnoe@gmail.com>
2022-04-02 09:56:43 +0300

    added closing modal by ESC

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import modalStyles from './modal.module.css';
@@ -8,7 +8,17 @@ import ModalOverlay from '../modal-overlay/modal-overlay';
 const modalRoot = document.getElementById('react-modals');
 
 const Modal = (props) => { 
-       
+  
+  useEffect(() => {
+
+    const closeEsc = (evt) => {
+      if (evt.key === "Escape" || evt.key === "Esc")  {
+        props.closing();
+      }
+    }
+    document.addEventListener('keyup', closeEsc); 
+  }, [props]);
+
   return ReactDOM.createPortal(
     (
       <>
--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -4,7 +4,6 @@ export const cardPropTypes = PropTypes.shape({
   image: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  //__v: PropTypes.number.isRequired,
   _id: PropTypes.string.isRequired,
   type: PropTypes.oneOf(['bun', 'main', 'sauce']).isRequired,
 });
