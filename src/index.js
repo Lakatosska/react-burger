895f98466230f6c09bfc4427bdbc5c2a8d06e10e
Lakatosska <vizualnoe@gmail.com>
2022-04-18 22:59:50 +0300

    enable Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import App from './components/app/app';
 import './index.css';
 import { rootReducer } from './services/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer)
+// Redux DevTools 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+// инициализируем хранилище
+const store = createStore(rootReducer, enhancer)
 
 
 ReactDOM.render(
