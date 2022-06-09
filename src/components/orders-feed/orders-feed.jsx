08b3e6f545ae10a1f5ba7c8310a5cf71d036bd92
Elena Nikiforova <vizualnoe@gmail.com>
2022-06-09 10:17:36 +0300

    added scroll to Feed page

--- src/components/orders-feed/orders-feed.jsx
+++ src/components/orders-feed/orders-feed.jsx
@@ -1,10 +1,12 @@
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
 import ordersFeedStyles from './orders-feed.module.css';
 
 
-
-export const CardOrder = ({card}) => {
+export const CardOrder = ({ card }) => {
 
   const { name, number, createdAt, ingredients: ingredientsId} = card;
 
@@ -15,9 +17,14 @@ export const CardOrder = ({card}) => {
     return ingredients.find(el => el._id === item);
   });
 
-  return (
-    <article className={ordersFeedStyles.card}>
+  const sumTotal = useMemo(() => {
+    return (
+      orderedIngredients.reduce((acc, item) => acc + item.price, 0)
+    );
+  }, [orderedIngredients]);
 
+  return (
+    <article className={`${ordersFeedStyles.card} mr-2`}>
       <div className={ordersFeedStyles.info}>
         <p className="text text_type_digits-default">#{number}</p>
         <p className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</p>
@@ -26,7 +33,6 @@ export const CardOrder = ({card}) => {
       <p className="text text_type_main-medium  mt-6 mb-6">{name}</p>
 
       <div className={ordersFeedStyles.total}>
-
         <ul className={ordersFeedStyles.icons}>
           { 
             (ingredientsId.length > 5) &&
@@ -46,12 +52,10 @@ export const CardOrder = ({card}) => {
         </ul>
 
         <div className={`${ordersFeedStyles.price} ml-6`}>
-          <p className="text text_type_digits-default mr-2">480</p>
+          <p className="text text_type_digits-default mr-2">{sumTotal}</p>
           <CurrencyIcon type="primary" />
         </div>
-
       </div>
-
     </article>
   )
 }
@@ -63,7 +67,7 @@ export const OrdersFeed = () => {
 
   return (
     <section>
-      <ul className={ordersFeedStyles.cardList}>
+      <ul className={`${ordersFeedStyles.cardList} custom-scroll`}>
         {orders.map(item => {
             return(
               <CardOrder card={item}/>
